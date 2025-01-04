1319cff688973d20201bf64f91cd4c23b1bfa5b5
Mahesh-8790 <maheshramtenki8@gmail.com>
2025-01-04 15:52:31 +0530

    main_method

--- Student_Project_Assignment/src/main/java/com/limasoft/StudentProjectAssignmentApplication.java
+++ Student_Project_Assignment/src/main/java/com/limasoft/StudentProjectAssignmentApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentProjectAssignmentApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		
 		SpringApplication.run(StudentProjectAssignmentApplication.class, args);
 	}
 
